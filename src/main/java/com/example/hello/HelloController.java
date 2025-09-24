e4a2f26810139437e8e80d80e2e7c8093f66bc2f
panagiotisnikolaou <pan.nik636@gmail.com>
2025-09-24 09:53:12 +0100

    added db test files

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!!!";
-	}
+    private final UserRepository userRepository;
 
+    public HelloController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "index"; // looks for src/main/resources/templates/index.html
+    }
+
+    @PostMapping("/add")
+    public String addUser(@RequestParam String name) {
+        User user = new User();
+        user.setName(name);
+        userRepository.save(user);
+        return "redirect:/";
+    }
 }
